103f3ffa78b1fa96b819ec252a01b9b12ef1b6b6
XiaoPangxie732 <47449269+XiaoPangxie732@users.noreply.github.com>
2023-07-21 23:23:49 +0800

    Initial commit

---
+++ src/forge/java/cn/maxpixel/mods/wuziqi/WuziqiMod.java
@@ -0,0 +1,11 @@
+package cn.maxpixel.mods.wuziqi;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(WuziqiMod.MODID)
+public class WuziqiMod {
+    public static final String MODID = "wuziqi";
+
+    public WuziqiMod() {
+    }
+}
